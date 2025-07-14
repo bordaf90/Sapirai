bd92666150ae81eaf56fa2839f90bfd2ce459fe1
Abraham Borda <44656558+bordaf90@users.noreply.github.com>
2025-07-14 10:12:27 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Sapirai Solutions - AI for your business" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Sapirai Solutions - AI for your business" />
       </Head>
       <body>
         <Main />
