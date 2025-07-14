43ea15837dcf0d8cf985dea5a41cd971ce73b7b7
Abraham Borda <44656558+bordaf90@users.noreply.github.com>
2025-07-14 09:52:45 -0300

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Sapirai Solutions - AI for your business" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
